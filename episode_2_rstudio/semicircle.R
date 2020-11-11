cc0562233d4114dfe654e5695de635797d60dfde
James Smith <james.smith2@ndorms.ox.ac.uk>
2020-11-11 13:51:49 +0000

    Initial commit

---
+++ episode_2_rstudio/semicircle.R
@@ -0,0 +1,4 @@
+x <- seq.int(from = -1, to = 1,length.out = 100)
+y <- sqrt(1-x**2) # ** is same as ^
+a <- plot(x,y)
+
---
+++ episode_2_rstudio/start_again.R
@@ -0,0 +1,20 @@
+#start again 
+
+x <- rnorm(10)
+
+sum <- 0
+
+for(i in 1:length(x)){
+  sum <- sum + x[[i]]
+}
+
+print(sum)
+
+z <- runif(n = 10,min = 1,max = 2)
+
+# I love R
+# I love R
+# I love R
+# I love R
+# I love R
+
